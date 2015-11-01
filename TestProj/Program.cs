705e21f0f6ab6707fee325a9498e1f66f57357a5
huxqgit <532123339@qq.com>
2015-11-01 23:36:13 +0800

    常规更新

--- TestProj/Program.cs
+++ TestProj/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ServiceProcess;
 
 namespace TestProj
 {
@@ -9,10 +10,19 @@ namespace TestProj
 	{
 		static void Main(string[] args)
 		{
-			//服务器测试修改，客户端同步
-			//客户端测试修改，服务端同步
-			//服务端修改，客户端同步测试
-			//客户端修改，服务端同步测试
+            ////定时任务
+            //TestService tServer = new TestService();
+            //tServer.TimedTask();
+            //return;
+
+
+            //后台服务
+            ServiceBase[] serv = new ServiceBase[]
+            {
+                new TestService()
+            };
+
+            ServiceBase.Run(serv);
 		}
 	}
 }
---
+++ TestProj/TestService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Timers;
+using LogHelper;
+
+namespace TestProj
+{
+    partial class TestService : ServiceBase
+    {
+        System.Timers.Timer _timer;
+
+        public TestService()
+        {
+            InitializeComponent();
+
+            _timer = new System.Timers.Timer();
+            _timer.Interval = int.Parse(System.Configuration.ConfigurationManager.AppSettings["时间间隔(单位分钟)"]) * 60000;
+            _timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
+
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            SysLogger.Instance.WriteErrorLog(string.Format(@"致命错误:{0}", e.ExceptionObject.ToString()));
+        }
+
+        public void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 定时任务执行
+        /// </summary>
+        public void TimedTask()
+        {
+
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // TODO: 在此处添加代码以启动服务。
+        }
+
+        protected override void OnStop()
+        {
+            // TODO: 在此处添加代码以执行停止服务所需的关闭操作。
+        }
+    }
+}
